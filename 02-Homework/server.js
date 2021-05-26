816a4e4ea403535cde44ab949badb0e7461c5bca
teddy kasamani <tykasamani@gmail.com>
2021-05-26 09:52:47 +0000

    new file added

--- 02-Homework/server.js
+++ 02-Homework/server.js
@@ -3,7 +3,10 @@ const express = require('express')
 const app = express();
 const path =require('path');
 const PORT = 3000;
-
+const fs = require ('fs');
+const { error } = require('console');
+const { response } = require('express');
+const { ERANGE } = require('constants');
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -16,5 +19,21 @@ app.get('/', (req, res) => res.sendFile(path.join(__dirname, './Develop/public/i
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './Develop/public/notes.html')));
 
 app.get('/api/notes', (req, res) => res.sendFile(path.join(__dirname, './Develop/db/db.json')));
+
+app.post('/api/notes', (req, res) =>{let newnote = req.body
+    fs.readFile(path.join(__dirname,'./Develop/db/db.json'),(error,response)=>{
+        if (error){
+            console.log(error)
+        }
+        let jsonResponce = JSON.parse(response)
+        jsonResponce.push(newnote)
+        fs.writeFile(path.join(__dirname, './Develop/db/db.json'),JSON.stringify(jsonResponce),(error)=>{
+        if (error){
+            console.log(error)}
+        }
+        
+        )
+    })
+})
 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
